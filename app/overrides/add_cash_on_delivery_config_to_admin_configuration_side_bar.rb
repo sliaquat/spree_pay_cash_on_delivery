a530302688b89727f659c31b8a3e4db7e8e30404
Sanad Liaquat <sanadhussain@gmail.com>
2015-03-10 16:54:51 +0500

    bug fixes

--- app/overrides/add_cash_on_delivery_config_to_admin_configuration_side_bar.rb
+++ app/overrides/add_cash_on_delivery_config_to_admin_configuration_side_bar.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(virtual_path: "spree/admin/shared/_configuration_menu",
                      name: "cash_on_delivery_config_settings_added_to_configuration_menu",
-                     insert_bottom: ".sidebar",
+                     insert_bottom: "[data-hook='admin_configurations_sidebar_menu']",
                      partial: "spree/admin/shared/cash_on_delivery_config_settings"
 )
